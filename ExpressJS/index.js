e17c2fb24eaa884fe399cc9621cee7a40c85c182
”Dunnikov <dynnikovleha@gmail.com>
2021-12-13 12:02:00 +0300

    add form

--- ExpressJS/index.js
+++ ExpressJS/index.js
@@ -17,6 +17,9 @@ app.set('view engine', 'hbs');
 app.set('views', 'views');
 
 app.use(express.static('public'));
+app.use(express.urlencoded({
+    extended: true
+}))
 //Registration rotes
 app.use('/', homeRoutes);
 app.use('/add', addRoutes);
