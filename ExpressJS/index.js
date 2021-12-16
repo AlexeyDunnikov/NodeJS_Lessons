d4259603b2a0ce87045bd62bc7ee51176006fe3d
”Dunnikov <dynnikovleha@gmail.com>
2021-12-16 11:32:23 +0300

    add user

--- ExpressJS/index.js
+++ ExpressJS/index.js
@@ -6,6 +6,7 @@ const homeRoutes = require("./routes/home");
 const addRoutes = require("./routes/add");
 const coursesRoutes = require("./routes/courses");
 const cardRoutes = require("./routes/card");
+const User = require("./models/user");
 
 const Handlebars = require("handlebars");
 const {
@@ -24,6 +25,16 @@ app.engine("hbs", hbs.engine);
 app.set("view engine", "hbs");
 app.set("views", "views");
 
+app.use(async (req, res, next) => {
+  try {
+    const user = await User.findById("61baf4bc3f36398550de9a73");
+    req.user = user;
+    next();
+  } catch (err) {
+    console.log(err);
+  }
+});
+
 app.use(express.static(path.join(__dirname, "public")));
 app.use(
   express.urlencoded({
@@ -41,7 +52,19 @@ const PORT = process.env.PORT || 3000;
 async function start() {
   try {
     const url = `mongodb+srv://Alexey:Jr7RTFqcyHei6gyh@cluster0.bersp.mongodb.net/shop`;
-    await mongoose.connect(url, { useNewUrlParser: true});
+    await mongoose.connect(url, { useNewUrlParser: true });
+
+    const candidate = await User.findOne();
+    if (!candidate) {
+      const user = new User({
+        email: "dunnikov223@gmail.com",
+        name: "Alexey",
+        cart: {
+          items: [],
+        },
+      });
+      await user.save();
+    }
 
     app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
