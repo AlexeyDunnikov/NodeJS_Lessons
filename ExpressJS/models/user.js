b031554f77c37f70e9a7cacd7dede17bc54f6843
”Dunnikov <dynnikovleha@gmail.com>
2021-12-16 13:36:03 +0300

    add to basket

--- ExpressJS/models/user.js
+++ ExpressJS/models/user.js
@@ -18,13 +18,32 @@ const userSchema = new Schema({
           default: 1,
         },
         courseId: {
-            type: Schema.Types.ObjectId,
-            ref: 'Course',
-            required: true,
-        }
+          type: Schema.Types.ObjectId,
+          ref: "Course",
+          required: true,
+        },
       },
     ],
   },
 });
+
+userSchema.methods.addToCart = function (course) {
+  const clonedItems = [...this.cart.items];
+  const ind = clonedItems.findIndex((c) => {
+    return c.courseId.toString() === course._id.toString();
+  });
+
+  if (ind >= 0) {
+    clonedItems[ind].count = clonedItems[ind].count + 1;
+  } else {
+    clonedItems.push({
+      count: 1,
+      courseId: course._id,
+    });
+  }
+
+  this.cart = {items: clonedItems};
+  return this.save();
+};
 
 module.exports = model("User", userSchema);
